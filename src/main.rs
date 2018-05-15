b4b74ca5fa8036c3923421855df4439d33422cef
Justin Barclay <justincbarclay@gmail.com>
2018-05-14 20:51:35 -0600

    Simplify user of futures and output a slack message

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ extern crate pretty_env_logger;
 extern crate tokio_core;
 extern crate hyper_tls;
 extern crate serde_json;
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
 
 use futures::Stream;
 use futures::Future;
@@ -18,9 +22,40 @@ use hyper::server::{Http, Service, Request, Response};
 
 static NOTFOUND: &[u8] = b"Not Found";
 
-//pub type ResponseStream = Bkox<Stream<Item = Chunk, Error = Error>>;
-
 struct ResponseExample(tokio_core::reactor::Handle);
+
+#[derive(Serialize, Deserialize)]
+struct Attachment{
+  title: String,
+  image_url: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct SlackMessage{
+  channel: String,
+  attachments: [Attachment; 1],
+}
+
+fn parse_response(body: &Chunk) -> Result<String, Error> {
+  let v: Value = serde_json::from_slice(&body).unwrap();
+  let parsed_result = v["data"]["children"][0]["data"]["url"].to_string();
+  if parsed_result.is_empty() || parsed_result == "null"  {
+    Err(Error::Status)
+  } else {
+    Ok(parsed_result)
+  }
+}
+
+fn make_slack_response(url: String) -> String {
+  let attachment = Attachment {
+    title: "Someone is panicing".to_string(),
+    image_url: "url".to_string()
+  };
+
+  let message = SlackMessage {channel:"#general".to_string(),
+                              attachments: [attachment]};
+  serde_json::to_string(&message).unwrap()
+}
 
 impl Service for ResponseExample {
   type Request = Request;
@@ -42,20 +77,16 @@ impl Service for ResponseExample {
         Box::new(web_res_future.and_then(|web_res| {
 
           web_res.body().concat2().and_then( move |body| {
-            let v: Value = serde_json::from_slice(&body).unwrap();
-            let url = v["data"]["children"][0]["data"]["url"].to_string();
-            Box::new(
-              futures::future::ok(
-                Response::new()
-                  .with_status(StatusCode::Ok)
-                  .with_body(url))
-            )
+            let slack_message = match parse_response(&body){
+              Ok(response) => make_slack_response(response),
+              Err(_e) => "Error".to_string()
+            };
+            println!("{:?}", slack_message);
+            Ok(
+              Response::new()
+                .with_status(StatusCode::Ok)
+                .with_body(slack_message))
           })
-            .map(|x| {
-              x
-            })
-        }).map(|x| {
-          x
         }))
       }
       _ => {
@@ -73,7 +104,6 @@ fn main() {
   pretty_env_logger::init();
 
   let mut addr: std::net::SocketAddr = "127.0.0.1:3000".parse().unwrap();
-  //  let port: i32 = std::env::args("RUST_PORT").parse().unwrap();
 
   let port = match std::env::var("RUST_PORT") {
     Ok(val) => val.parse::<u16>().unwrap(),
