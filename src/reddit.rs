6c72daa28e24279e8b5b9d126b3cbca9598f30a3
Justin Barclay <justincbarclay@gmail.com>
2018-06-07 21:27:23 -0600

    Add file for processing reddit json

---
+++ src/reddit.rs
@@ -0,0 +1,84 @@
+
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+extern crate hyper_tls;
+extern crate serde_json;
+extern crate serde;
+
+use futures::Stream;
+use futures::Future;
+
+use serde_json::Value;
+
+use self::hyper::{Chunk, Client, Get, StatusCode};
+use hyper_tls::HttpsConnector;
+use self::hyper::error::Error as Error;
+use self::hyper::server::{Request, Response};
+
+#[derive(Serialize, Deserialize)]
+struct Attachment {
+  title: String,
+  image_url: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SlackMessage {
+  channel: String,
+  attachments: [Attachment; 1],
+}
+
+fn parse_response(body: &Chunk) -> ::std::result::Result<String, Error> {
+  let v: Value = serde_json::from_slice(&body).unwrap();
+  let parsed_result = v["data"]["children"][0]["data"]["url"].to_string();
+  if parsed_result.is_empty() || parsed_result == "null" {
+    Err(hyper::error::Error::Status)
+  } else {
+    Ok(parsed_result)
+  }
+}
+
+fn make_slack_response(url: String) -> String {
+  let attachment = Attachment {
+    title: "Someone is panicing".to_string(),
+    image_url: url.to_string(),
+  };
+
+  let message = SlackMessage {
+    channel: "#general".to_string(),
+    attachments: [attachment],
+  };
+  serde_json::to_string(&message).unwrap()
+}
+
+pub fn get_top_aww_post(
+  handler: &tokio_core::reactor::Handle,
+) -> Box<Future<Item = hyper::Response, Error = hyper::Error>> {
+  let client = Client::configure()
+    .connector(HttpsConnector::new(4, handler).unwrap())
+    .build(handler);
+  let req = Request::new(
+    Get,
+    "https://www.reddit.com/r/aww/top/.json?limit=1"
+      .parse()
+      .unwrap(),
+  );
+  let web_res_future = client.request(req);
+
+  Box::new(web_res_future.and_then(|web_res| {
+    web_res.body().concat2().and_then(move |body| {
+      let slack_message = match parse_response(&body) {
+        Ok(response) => {
+          make_slack_response(response)
+        },
+        Err(_e) => "Error parsing JSON".to_string(),
+      };
+      println!("{:?}", slack_message);
+      Ok(
+        Response::new()
+          .with_status(StatusCode::Ok)
+          .with_body(slack_message),
+      )
+    })
+  }))
+}
